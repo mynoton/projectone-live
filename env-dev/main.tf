2c3fff7b24dc56d69af490fb984fb0208846ddf2
mynoton <41176437+mynoton@users.noreply.github.com>
2018-07-26 20:39:45 +0700

    update

--- env-dev/main.tf
+++ env-dev/main.tf
@@ -58,12 +58,12 @@ module "azure_resource_group_b" {
 #================================================================
 
 module "azure_virtual_network" {
-  source                  = "git::https://github.com/mynoton/projectone-module.git//modules/azure_virtual_network"
-  vnw_name                = "VNW ProjectOne DEV"
-  vnw_address             = "10.0.0.0/16"
-  vnw_location            = "Southeast Asia"
-  vnw_tag_env             = "VNW ProjectOne DEV"
-  vnw_resource_group_name = "${module.azure_resource_group_a.resource_group_name_output}"
+  source                   = "git::https://github.com/mynoton/projectone-module.git//modules/azure_virtual_network"
+  vnet_name                = "VNW ProjectOne DEV"
+  vnet_address             = "10.0.0.0/16"
+  vnet_location            = "Southeast Asia"
+  vnet_tag_env             = "VNW ProjectOne DEV"
+  vnet_resource_group_name = "${module.azure_resource_group_a.resource_group_name_output}"
 }
 
 #================================================================
@@ -90,40 +90,14 @@ module "azure_subnet_db" {
 # Azure Public IP Provisioning
 #================================================================
 
-module "azure_public_ip_app01" {
+module "azure_public_ip" {
   source                        = "git::https://github.com/mynoton/projectone-module.git//modules/azure_public_ip"
-  public_ip_name                = "Public IP ProjectOne APP01 DEV"
+  public_ip_name                = "Public IP ProjectOne DEV"
   public_ip_location            = "Southeast Asia"
   public_ip_resource_group_name = "${module.azure_resource_group_a.resource_group_name_output}"
   public_ip_type                = "static"
-  public_ip_tag_environment     = "Public IP ProjectOne APP01 DEV"
-}
-
-module "azure_public_ip_app02" {
-  source                        = "git::https://github.com/mynoton/projectone-module.git//modules/azure_public_ip"
-  public_ip_name                = "Public IP ProjectOne APP02 DEV"
-  public_ip_location            = "Southeast Asia"
-  public_ip_resource_group_name = "${module.azure_resource_group_a.resource_group_name_output}"
-  public_ip_type                = "static"
-  public_ip_tag_environment     = "Public IP ProjectOne APP02 DEV"
-}
-
-module "azure_public_ip_db01" {
-  source                        = "git::https://github.com/mynoton/projectone-module.git//modules/azure_public_ip"
-  public_ip_name                = "Public IP ProjectOne DB01 DEV"
-  public_ip_location            = "Southeast Asia"
-  public_ip_resource_group_name = "${module.azure_resource_group_a.resource_group_name_output}"
-  public_ip_type                = "static"
-  public_ip_tag_environment     = "Public IP ProjectOne DB01 DEV"
-}
-
-module "azure_public_ip_db02" {
-  source                        = "git::https://github.com/mynoton/projectone-module.git//modules/azure_public_ip"
-  public_ip_name                = "Public IP ProjectOne DB02 DEV"
-  public_ip_location            = "Southeast Asia"
-  public_ip_resource_group_name = "${module.azure_resource_group_a.resource_group_name_output}"
-  public_ip_type                = "static"
-  public_ip_tag_environment     = "Public IP ProjectOne DB02 DEV"
+  public_ip_dns_name            = "projectonedev"
+  public_ip_tag_environment     = "Public IP ProjectOne DEV"
 }
 
 #================================================================
@@ -151,52 +125,34 @@ module "azure_network_security_group" {
 # Azure Virtual Network Interface Provisioning
 #================================================================
 
-module "azure_virtual_network_interface_app01" {
-  source                    = "git::https://github.com/mynoton/projectone-module.git//modules/azure_virtual_network_interface"
-  vnic_name                 = "VNIC APP01 ProjectOne DEV"
-  vnic_location             = "Southeast Asia"
-  vnic_resource_group_name  = "${module.azure_resource_group_a.resource_group_name_output}"
-  vnic_conf_name            = "VNIC APP01 ProjectOne DEV Configuration"
-  vnic_conf_subnet_id       = "${module.azure_subnet_app.subnet_id_output}"
-  vnic_conf_private_ip_type = "static"
-  vnic_conf_public_ip_id    = "${module.azure_public_ip_app01.public_ip_id_output}"
-  vnic_tag_env              = "VNIC APP01 ProjectOne DEV"
+module "azure_virtual_network_interface_app" {
+  source                       = "git::https://github.com/mynoton/projectone-module.git//modules/azure_virtual_network_interface"
+  vnic_name                    = "VNIC-APP-ProjectOne-DEV"
+  vnic_location                = "Southeast Asia"
+  vnic_resource_group_name     = "${module.azure_resource_group_a.resource_group_name_output}"
+  vnic_count                   = 2
+  vnic_conf_name               = "VNIC-APP-ProjectOne-DEV-Configuration"
+  vnic_conf_subnet_id          = "${module.azure_subnet_app.subnet_id_output}"
+  vnic_conf_private_ip_type    = "static"
+  #vnic_conf_public_ip_id      = "${module.azure_public_ip_app.public_ip_id_output}"
+  vnic_conf_lb_bk_address_pool = "${module.azure_loadbalancer.lb_backend_address_pool_id_output}"
+  vnic_conf_lb_nat_rule_id     = "${module.azure_loadbalancer.azurerm_lb_nat_rule_id}" 
+  vnic_tag_env                 = "VNIC-APP-ProjectOne-DEV"
 }
 
-module "azure_virtual_network_interface_app02" {
-  source                    = "git::https://github.com/mynoton/projectone-module.git//modules/azure_virtual_network_interface"
-  vnic_name                 = "VNIC APP02 ProjectOne DEV"
-  vnic_location             = "Southeast Asia"
-  vnic_resource_group_name  = "${module.azure_resource_group_a.resource_group_name_output}"
-  vnic_conf_name            = "VNIC APP02 ProjectOne DEV Configuration"
-  vnic_conf_subnet_id       = "${module.azure_subnet_app.subnet_id_output}"
-  vnic_conf_private_ip_type = "static"
-  vnic_conf_public_ip_id    = "${module.azure_public_ip_app02.public_ip_id_output}"
-  vnic_tag_env              = "VNIC APP02 ProjectOne DEV"
-}
-
-module "azure_virtual_network_interface_db01" {
-  source                    = "git::https://github.com/mynoton/projectone-module.git//modules/azure_virtual_network_interface"
-  vnic_name                 = "VNIC DB01 ProjectOne DEV"
-  vnic_location             = "Southeast Asia"
-  vnic_resource_group_name  = "${module.azure_resource_group_a.resource_group_name_output}"
-  vnic_conf_name            = "VNIC DB02 ProjectOne DEV Configuration"
-  vnic_conf_subnet_id       = "${module.azure_subnet_app.subnet_id_output}"
-  vnic_conf_private_ip_type = "static"
-  vnic_conf_public_ip_id    = "${module.azure_public_ip_db01.public_ip_id_output}"
-  vnic_tag_env              = "VNIC DB01 ProjectOne DEV"
-}
-
-module "azure_virtual_network_interface_db02" {
-  source                    = "git::https://github.com/mynoton/projectone-module.git//modules/azure_virtual_network_interface"
-  vnic_name                 = "VNIC DB02 ProjectOne DEV"
-  vnic_location             = "Southeast Asia"
-  vnic_resource_group_name  = "${module.azure_resource_group_a.resource_group_name_output}"
-  vnic_conf_name            = "VNIC DB02 ProjectOne DEV Configuration"
-  vnic_conf_subnet_id       = "${module.azure_subnet_app.subnet_id_output}"
-  vnic_conf_private_ip_type = "static"
-  vnic_conf_public_ip_id    = "${module.azure_public_ip_db02.public_ip_id_output}"
-  vnic_tag_env              = "VNIC DB02 ProjectOne DEV"
+module "azure_virtual_network_interface_db" {
+  source                       = "git::https://github.com/mynoton/projectone-module.git//modules/azure_virtual_network_interface"
+  vnic_name                    = "VNIC-DB-ProjectOne-DEV"
+  vnic_location                = "Southeast Asia"
+  vnic_resource_group_name     = "${module.azure_resource_group_a.resource_group_name_output}"
+  vnic_count                   = 2
+  vnic_conf_name               = "VNIC-DB-ProjectOne-DEV-Configuration"
+  vnic_conf_subnet_id          = "${module.azure_subnet_db.subnet_id_output}"
+  vnic_conf_private_ip_type    = "static"
+  #vnic_conf_public_ip_id      = "${module.azure_public_ip_db.public_ip_id_output}"
+  vnic_conf_lb_bk_address_pool = "${module.azure_loadbalancer.lb_backend_address_pool_id_output}"
+  vnic_conf_lb_nat_rule_id     = "${module.azure_loadbalancer.azurerm_lb_nat_rule_id}"
+  vnic_tag_env                 = "VNIC-DB-ProjectOne-DEV"
 }
 
 #================================================================
@@ -217,14 +173,16 @@ module "azure_storage_account_boot_diagnostic" {
 # Azure Virtual Machine Provisioning
 #================================================================
 
-module "azure_virtual_machine_app01" {
-  source                     = "git::https://github.com/mynoton/projectone-module.git//modules/azure_virtual_machine"
-  vm_name                    = "VM APP01"
+module "azure_virtual_machine_app" {
+  source                     = "git::https://github.com/mynoton/projectone-module.git//modules/azure_virtual_machine_avset"
+  vm_name                    = "VM-APP-DEV"
   vm_location                = "Southeast Asia"
   vm_resource_group_name     = "${module.azure_resource_group_a.resource_group_name_output}"
-  vm_vnic_id                 = "${module.azure_virtual_network_interface_app01.virtual_network_interface_id_output}"
+  vm_vnic_id                 = ["${module.azure_virtual_network_interface_app.virtual_network_interface_id_output}"]
   vm_size                    = "Standard_DS1_v2"
-  vm_disk_name               = "VM Disk APP01"
+  vm_avset_id                = "${module.azure_availability_set.availability_set_id_output}"
+  vm_instance_count          = 2
+  vm_disk_name               = "VM-Disk-APP-DEV"
   vm_disk_caching            = "ReadWrite"
   vm_disk_create_option      = "FromImage"
   vm_disk_type               = "Premium_LRS"
@@ -237,17 +195,19 @@ module "azure_virtual_machine_app01" {
   vm_config_sshkey_path      = "/home/azureuser/.ssh/authorized_keys"
   vm_config_sshkey_data      = "ssh-rsa AAAAB3Nz{snip}hwhqT9h"
   vm_diagnostic_storage_uri  = "${module.azure_storage_account_boot_diagnostic.storage_account_endpoint_output}"
-  vm_tag_env                 = "VM APP01"
+  vm_tag_env                 = "VM-APP-DEV"
 }
 
-module "azure_virtual_machine_app02" {
+module "azure_virtual_machine_db" {
   source                     = "git::https://github.com/mynoton/projectone-module.git//modules/azure_virtual_machine"
-  vm_name                    = "VM APP02"
+  vm_name                    = "VM-DB-DEV"
   vm_location                = "Southeast Asia"
   vm_resource_group_name     = "${module.azure_resource_group_a.resource_group_name_output}"
-  vm_vnic_id                 = "${module.azure_virtual_network_interface_app02.virtual_network_interface_id_output}"
+  vm_vnic_id                 = ["${module.azure_virtual_network_interface_db.virtual_network_interface_id_output}"]
   vm_size                    = "Standard_DS1_v2"
-  vm_disk_name               = "VM Disk APP02"
+  vm_avset_id                = "${module.azure_availability_set.availability_set_id_output}"
+  vm_instance_count          = 2
+  vm_disk_name               = "VM-Disk-DB-DEV"
   vm_disk_caching            = "ReadWrite"
   vm_disk_create_option      = "FromImage"
   vm_disk_type               = "Premium_LRS"
@@ -260,51 +220,57 @@ module "azure_virtual_machine_app02" {
   vm_config_sshkey_path      = "/home/azureuser/.ssh/authorized_keys"
   vm_config_sshkey_data      = "ssh-rsa AAAAB3Nz{snip}hwhqT9h"
   vm_diagnostic_storage_uri  = "${module.azure_storage_account_boot_diagnostic.storage_account_endpoint_output}"
-  vm_tag_env                 = "VM APP02"
+  vm_tag_env                 = "VM-DB-DEV"
 }
 
-module "azure_virtual_machine_db01" {
-  source                     = "git::https://github.com/mynoton/projectone-module.git//modules/azure_virtual_machine"
-  vm_name                    = "VM DB01"
-  vm_location                = "Southeast Asia"
-  vm_resource_group_name     = "${module.azure_resource_group_a.resource_group_name_output}"
-  vm_vnic_id                 = "${module.azure_virtual_network_interface_db01.virtual_network_interface_id_output}"
-  vm_size                    = "Standard_DS1_v2"
-  vm_disk_name               = "VM Disk DB01"
-  vm_disk_caching            = "ReadWrite"
-  vm_disk_create_option      = "FromImage"
-  vm_disk_type               = "Premium_LRS"
-  vm_image_publisher         = "Canonical"
-  vm_image_offer             = "UbuntuServer"
-  vm_image_sku               = "16.04.0-LTS"
-  vm_image_version           = "latest"
-  vm_profile_com_name        = "myvm"
-  vm_profile_admin_username  = "azureuser"
-  vm_config_sshkey_path      = "/home/azureuser/.ssh/authorized_keys"
-  vm_config_sshkey_data      = "ssh-rsa AAAAB3Nz{snip}hwhqT9h"
-  vm_diagnostic_storage_uri  = "${module.azure_storage_account_boot_diagnostic.storage_account_endpoint_output}"
-  vm_tag_env                 = "VM DB01"
+#================================================================
+# Azure Virtual Machine Provisioning
+#================================================================
+
+module "azure_availability_set" {
+  source                    = "git::https://github.com/mynoton/projectone-module.git//modules/azure_availability_set"
+  avset_name                = "Availability Set ProjectOne DEV"
+  avset_location            = "Southeast Asia"
+  avset_resource_group_name = "${module.azure_resource_group_a.resource_group_name_output}"
+  avset_fault_count         = 2
+  avset_update_count        = 2
+  avset_management          = true
 }
 
-module "azure_virtual_machine_db02" {
-  source                     = "git::https://github.com/mynoton/projectone-module.git//modules/azure_virtual_machine"
-  vm_name                    = "VM DB02"
-  vm_location                = "Southeast Asia"
-  vm_resource_group_name     = "${module.azure_resource_group_a.resource_group_name_output}"
-  vm_vnic_id                 = "${module.azure_virtual_network_interface_db02.virtual_network_interface_id_output}"
-  vm_size                    = "Standard_DS1_v2"
-  vm_disk_name               = "VM Disk DB02"
-  vm_disk_caching            = "ReadWrite"
-  vm_disk_create_option      = "FromImage"
-  vm_disk_type               = "Premium_LRS"
-  vm_image_publisher         = "Canonical"
-  vm_image_offer             = "UbuntuServer"
-  vm_image_sku               = "16.04.0-LTS"
-  vm_image_version           = "latest"
-  vm_profile_com_name        = "myvm"
-  vm_profile_admin_username  = "azureuser"
-  vm_config_sshkey_path      = "/home/azureuser/.ssh/authorized_keys"
-  vm_config_sshkey_data      = "ssh-rsa AAAAB3Nz{snip}hwhqT9h"
-  vm_diagnostic_storage_uri  = "${module.azure_storage_account_boot_diagnostic.storage_account_endpoint_output}"
-  vm_tag_env                 = "VM DB02"
+#================================================================
+# Azure Load Balancer Provisioning
+#================================================================
+
+module "azure_loadbalancer" {
+  source                                 = "git::https://github.com/mynoton/projectone-module.git//modules/azure_loadbalancer"
+  lb_resource_group_name                 = "${module.azure_resource_group_a.resource_group_name_output}"
+  lb_name                                = "LB-ProjectOne-DEV"
+  lb_location                            = "Southeast Asia"
+  lb_fe_conf_name                        = "LB-FE-ProjectOne-DEV"
+  lb_fe_conf_public_ip_id                = "${module.azure_public_ip.public_ip_id_output}"
+  lb_bk_address_pool_resource_group_name = "${module.azure_resource_group_a.resource_group_name_output}"
+  lb_bk_address_pool_name                = "LB-BKPool-ProjectOne-DEV"
+  lb_nat_rule_resource_group_name        = "${module.azure_resource_group_a.resource_group_name_output}"
+  lb_nat_rule_name                       = "LB-NAT-RDP-VM-ProjectOne-DEV"
+  lb_nat_rule_protocol                   = "tcp"
+  # frontend port + count
+  lb_nat_rule_fe_port                    = "5000"
+  lb_nat_rule_bk_port                    = 3389
+  lb_nat_rule_fe_conf_name               = "LB-NAT-FE-Rule-ProjectOne-DEV"
+  lb_nat_rule_count                      = 2
+  lb_rule_resource_group_name            = "${module.azure_resource_group_a.resource_group_name_output}"
+  lb_rule_name                           = "LB-Rule-ProjectOne-DEV"
+  lb_rule_protocol                       = "tcp"
+  lb_rule_fe_port                        = "80"
+  lb_rule_bk_port                        = "80"
+  lb_rule_fe_conf_name                   = "LB-FE-Rule-ProjectOne-DEV"
+  lb_rule_enable_floating_ip             = false
+  lb_rule_idle_timeout_in_minutes        = 5
+  lb_probe_resource_group_name           = "${module.azure_resource_group_a.resource_group_name_output}"
+  lb_probe_name                          = "LB-TCP-Probe"
+  lb_probe_protocol                      = "tcp"
+  lb_probe_port                          = 80
+  lb_probe_interval_sec                  = 5
+  lb_probe_number_probe                  = 2
 }
+
